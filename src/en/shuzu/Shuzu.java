0407b9a185c753403269eea6aa6c2c42ae7d621d
EGua <554326737@qq.com>
2018-01-02 16:25:23 +0800

    实训周

---
+++ src/en/shuzu/Shuzu.java
@@ -0,0 +1,20 @@
+package en.shuzu;
+
+import java.util.Scanner;
+
+public class Shuzu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner sc=new Scanner(System.in);
+    int[] arr;
+    System.out.println("");
+    int a=sc.nextInt();
+    arr=new int[a];
+    for (int i = 0; i < arr.length; i++) {
+		System.out.println("¹þ");
+	}
+    
+	}
+
+}
